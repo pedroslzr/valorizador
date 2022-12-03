0db6275f836203436d5ca3715ba0112f23b5ef3d
pedroslzr <pedro.salazar@pucp.pe>
2022-12-03 14:57:16 -0500

    03.12

---
+++ README.py
@@ -0,0 +1,11 @@
+# Valorizador
+
+este proyecto busca crear una boleta de valorizacion a partir de datos tomados de campo
+
+para correr este proyecto, llenar el archivo data, guardar con nombre data.csv y ejecutar la aplicacion
+
+'''
+python src/app.py
+
+'''
+la salida es el archivo valorizacion.xlsx en la carpeta src/out/valorizacion.xlsx
