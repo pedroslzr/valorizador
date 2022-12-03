82f19fa2c0baae9f11571bcacb7ee13b5e0b3539
pedroslzr <pedro.salazar@pucp.pe>
2022-12-03 14:30:47 -0500

    entrega 03.12

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ df = df.fillna(0)
 
 # Importo datos del membrete
 df2 = pd.read_csv("./data/data.csv", sep=";", encoding='utf-8-sig', index_col=False, header=None)
-df2 = df2.iloc[0:13,0:2]
+df2 = df2.iloc[0:14,0:2]
 df2 = df2.fillna(0)
 
 # Crear columnas
@@ -82,23 +82,21 @@ pracum = '{:.2%}'.format(coacum / presupuesto) #formatea a % con 2 puntos decima
 cosald = round_2dec(df["costsald"].sum())
 prsald = '{:.2%}'.format(cosald / presupuesto) #formatea a % con 2 puntos decimales
 
-lista_sumas = [presupuesto,coant,coval,coacum,pracum,cosald,prsald]
-#lista_col_sumas = ['5','7','9','11','12','14','15']
-lista_col_sumas = [5,7,9,11,12,14,15]
+#lista_sumas = [presupuesto,coant,coval,coacum,pracum,cosald,prsald]
+lista_sumas = [presupuesto,coant,coval,coacum,cosald]
+lista_sumas_por = [pracum,prsald]
+
+lista_col_sumas = [5,7,9,11,14]
+lista_col_sumas_pr = [12,15]
+
 
 # Diccionario
 # Cambiamos el nombre de las columnas al dataframe original
 df.rename(columns = {"ppto": "PRESUPUESTO OFERTA S/.",
-                     "ACUMULADO ANTERIOR":"METRADO",
-                     "costant":"S/.",
-                     "AVANCE ACTUAL":"METRADO",
-                     "costval":"S/.",
-                     "macum":"METRADO",
-                     "costacum":"S/.", 
-                     "poracum":"%",
-                     "msald":"METRADO",
-                     "costsald":"S/.", 
-                     "porsald":"%"}, inplace = True)
+                     "ACUMULADO ANTERIOR":"METRADO","costant":"S/.",
+                     "AVANCE ACTUAL":"METRADO","costval":"S/.",
+                     "macum":"METRADO","costacum":"S/.", "poracum":"%",
+                     "msald":"METRADO","costsald":"S/.", "porsald":"%"}, inplace = True)
 
 
 
@@ -136,20 +134,20 @@ for idx2,element3 in enumerate(lista_anchos):
 worksheet.set_row(1, 40)
 
 # Formatos
-header_format = workbook.add_format({
-    "valign": "vcenter",
-    "align":"center",
-    "bg_color":"#951F06",
-    "bold":True,
-    "font_color":"#FFFFFF"})
-
 titulo_format = workbook.add_format({
     'bold': 0,
     'text_wrap': True,
     'border': 1,
     'align': 'center',
     'valign': 'vcenter',
-    #'fg_color': 'yellow'
+    })
+
+subtitulo_format = workbook.add_format({
+    'bold': 1,
+    'text_wrap': True,
+    'border': 0,
+    'align': 'center',
+    'valign': 'vcenter',
     })
 
 item_format = workbook.add_format({
@@ -160,11 +158,27 @@ item_format = workbook.add_format({
     'fg_color': '#C4D79B',
     'border': 1})
 
+total_fmt = workbook.add_format({'align': 'right', 'num_format': '$#,##0','bold': True, 'bottom':6})
+subtotal_fmt = workbook.add_format({'align': 'right', 'num_format': '$#,##0','bold': False})
+
+por_fmt = workbook.add_format({'align': 'right','bold': True})
+porc_fmt = workbook.add_format({'align': 'right','bold': False,'num_format': '#,##0.00"%"'})
+
+currency_format = workbook.add_format({'align': 'right','bold': True,'num_format': '"S/ "#,##0.00'})
+currency_format_sub = workbook.add_format({'align': 'right','bold': False,'num_format': '"S/ "#,##0.00'})
+
+number_format = workbook.add_format({'num_format': '#,##0.00'})
+
 # titulos
 title = str(df2.iloc[0][1])
+numero_val = str(df2.iloc[13][1])
+mes_val = str(df2.iloc[11][1])
+ano_val = str(df2.iloc[12][1])
+subtitulo = "VALORIZACION DE OBRA Nº:  "+numero_val+" CORRESPONDIENTE AL MES DE "+mes_val+ano_val
 worksheet.merge_range('A2:P2', title, titulo_format)
+worksheet.merge_range('B7:G7', subtitulo, subtitulo_format)
 
-# Indice de tabla
+# Encabezado de tabla
 lista_columnas = list(df.columns.values)
 lista_celdas = ["A9:A11","B9:B11","C9:C11","D9:D11","E9:E11","F9:F11","G10:G11","H10:H11","I10:I11","J10:J11","K10:K11","L10:L11","M10:M11","N10:N11","O10:O11","P10:P11"]
 for idx,element in enumerate(lista_columnas):
@@ -175,27 +189,57 @@ lista_subtitulos = ["ACUMULADO ANTERIOR","AVANCE FISICO","ACUMULADO ACTUAL","SAL
 for idx3,element4 in enumerate(lista_subtitulos):
     worksheet.merge_range(lista_columnas_merge[idx3], element4,item_format)
 
+# Sumas en el pie de pagina
 number_rows = df.shape[0] + 10
 
 for idx4,element5 in enumerate(lista_sumas):
-    #cell_location = xl_rowcol_to_cell(number_rows+1, lista_col_sumas[idx4])
-    #worksheet.write_number(cell_location, element5)
-    worksheet.write_string(number_rows+1,lista_col_sumas[idx4], str(element5))
+    worksheet.write_number(number_rows+1,lista_col_sumas[idx4], float(element5),currency_format)
 
-worksheet.write_string(number_rows+1, 1, "TOTAL COSTO DIRECTO")
+for idx5,element6 in enumerate(lista_sumas_por):
+    worksheet.write_string(number_rows+1,lista_col_sumas_pr[idx5], str(element6),por_fmt)
 
-#worksheet.merge_range(lista_celdas[0],lista_columnas[0])
+# Resultados pie de pagina
+gg = 0.0981015582855174
+ut = 0.08
+igv = 0.18
 
-#worksheet.merge_range('A9:A11',lista[0])
+lista_gg =list(map(round_2dec,[item*gg for item in lista_sumas]))
+for idx6,element7 in enumerate(lista_gg):
+    worksheet.write_number(number_rows+2,lista_col_sumas[idx6], float(element7),currency_format_sub)
+
+lista_ut =list(map(round_2dec,[item*ut for item in lista_sumas]))
+for idx7,element8 in enumerate(lista_ut):
+    worksheet.write_number(number_rows+3,lista_col_sumas[idx7], float(element8),currency_format_sub)
+
+lt_val_sin = [lista_sumas, lista_gg, lista_ut]
+lista_val_sin = sum(map(np.array, lt_val_sin))
+for idx8,element9 in enumerate(lista_val_sin):
+    worksheet.write_number(number_rows+4,lista_col_sumas[idx8], float(element9),currency_format)
+
+lista_igv =list(map(round_2dec,[item*igv for item in lista_val_sin]))
+for idx9,element10 in enumerate(lista_igv):
+    worksheet.write_number(number_rows+5,lista_col_sumas[idx9], float(element10),currency_format_sub)
+
+lt_val = [lista_val_sin, lista_igv]
+lista_val = sum(map(np.array, lt_val))
+for idx10,element11 in enumerate(lista_val):
+    worksheet.write_number(number_rows+6,lista_col_sumas[idx10], float(element11),currency_format)
 
 
-#df2 = pd.read_csv("./data/data.csv", sep=";", encoding='utf-8-sig', index_col=False)
-#df2 = df2.iloc[0:13,0:2]
-#df2 = df2.fillna(0)
-
-
-
-
-
+# Indice sumas
+worksheet.write_string(number_rows+1, 1, "TOTAL COSTO DIRECTO",total_fmt)
+worksheet.write_string(number_rows+2, 1, "GASTOS GENERALES",subtotal_fmt)
+worksheet.write_number(number_rows+2, 2, float(gg*100),porc_fmt)
+worksheet.write_string(number_rows+3, 1, "UTILIDAD",subtotal_fmt)
+worksheet.write_number(number_rows+3, 2, float(ut*100),porc_fmt)
+worksheet.write_string(number_rows+4, 1, "VALORIZACION MENSUAL (Sin I.G.V)",total_fmt)
+worksheet.write_string(number_rows+5, 1, "I.G.V",subtotal_fmt)
+worksheet.write_number(number_rows+5, 2, float(igv*100),porc_fmt)
+worksheet.write_string(number_rows+6, 1, "TOTAL A VALORIZAR EN EL MES (Incl. I.G.V)",total_fmt)
 
 writer.save()
+
+#TODO
+#Interpretar mas datos del membrete
+#crear base de datos que acumule datos
+#contrastar los avances con la programacion planificada
